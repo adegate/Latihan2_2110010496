b9ce6c33b8cb197f9fa99636e0d9870c0ea4c838
adegate <adegati365@gmail.com>
2023-06-13 20:09:50 +0800

    Membuat class mahasiswa dan MahasiswaBeraksi

---
+++ src/universitas/mahasiswa.java
@@ -0,0 +1,15 @@
+
+package universitas;
+
+public class mahasiswa {
+    String nama;
+    String npm;
+    String prodi;
+    
+    public  mahasiswa(String nama, String npm, String prodi){
+        this.nama = nama;
+        this.npm = npm;
+        this.prodi = prodi;
+        
+    }
+}
---
+++ src/universitas/mahasiswaBeraksi.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universitas;
+
+/**
+ *
+ * @author aoWAVE
+ */
+public class mahasiswaBeraksi {
+    public static void main(String[] args) {
+        
+        mahasiswa nama1 = new nama ("Ade");
+        mahasiswa npm1 = new npm("2110010496");
+        mahasiswa prodi = new prodi("TI");
+        
+        System.out.println("Nama : " + nama);
+        System.out.println("npm : " + npm);
+        System.out.println("prodi : " + prodi);
+    }
+}
