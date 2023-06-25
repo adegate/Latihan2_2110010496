5e6f39be4b7078254db4ab91ec67b1b63acd7ea3
adegate <adegati365@gmail.com>
2023-06-26 01:01:31 +0800

    Memperbaiki Program yang error

--- src/universitas/mahasiswa.java
+++ src/universitas/mahasiswa.java
@@ -6,10 +6,39 @@ public class mahasiswa {
     String npm;
     String prodi;
     
-    public  mahasiswa(String nama, String npm, String prodi){
+   String nama(){
+        return this.nama;
+    }
+    String npm(){
+        return this.npm;
+    }
+    String prodi(){
+        return this.prodi;
+    }
+    
+    mahasiswa (String nama , String npm , String prodi){
         this.nama = nama;
         this.npm = npm;
         this.prodi = prodi;
-        
+    }
+
+    public String getNama(){
+    return this.nama;
+    }
+    public String getNpm(){
+    return this.npm;
+    }
+    public String getProdi(){
+    return this.prodi;
+    }
+
+    public String setNama(){
+    return this.nama;
+    }
+    public String setNpm(){
+    return this.npm;
+    }
+    public String setProdi(){
+    return this.prodi;
     }
 }
--- src/universitas/mahasiswaBeraksi.java
+++ src/universitas/mahasiswaBeraksi.java
@@ -1,23 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universitas;
 
-/**
- *
- * @author aoWAVE
- */
 public class mahasiswaBeraksi {
     public static void main(String[] args) {
-        
-        mahasiswa nama1 = new nama ("Ade");
-        mahasiswa npm1 = new npm("2110010496");
-        mahasiswa prodi = new prodi("TI");
-        
-        System.out.println("Nama : " + nama);
-        System.out.println("npm : " + npm);
-        System.out.println("prodi : " + prodi);
+       mahasiswa data = new mahasiswa("Ade","2110010496","TI");
+     System.out.println("Nama  :" + data.getNama());
+     System.out.println("NPM   :" + data.getNpm());
+     System.out.println("Prodi :" + data.getProdi());
     }
 }
